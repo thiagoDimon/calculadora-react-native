62e0564c5b152ffd0b59f48fcc2c021252cb7a2f
thiagoDimon <othiago.miranda@outlook.com>
2024-05-11 15:07:11 -0300

    Atribui cálculos básicos

--- App.js
+++ App.js
@@ -1,47 +1,205 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'react-native-paper';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function App() {
+  const [resultado, setResultado] = useState([0])
+  const [ehPrimeiroNumero, setEhPrimeiroNumero] = useState(true);
+
+  function setResultadoTela(valor) {
+    if (valor === "C") {
+      setEhPrimeiroNumero(true);
+      return setResultado([0]);
+    }
+    if ((ehPrimeiroNumero && valor === 0) || 
+        (ehPrimeiroNumero && valor === "/") || 
+        (ehPrimeiroNumero && valor === "-") || 
+        (ehPrimeiroNumero && valor === "+") || 
+        (ehPrimeiroNumero && valor === "X")) {
+      return;
+    }
+    if (ehPrimeiroNumero) {
+      setEhPrimeiroNumero(false);
+      return setResultado([valor]);
+    }
+    return setResultado(oldResultado => [...oldResultado, valor]);
+  }
+
+  function calcularResultado() {
+    if (resultado.includes("+")) {
+      const resultadoFinal = resultado[0] + resultado[2];
+      setResultado([resultadoFinal]);
+    }
+    if (resultado.includes("-")) {
+      const resultadoFinal = resultado[0] - resultado[2];
+      setResultado([resultadoFinal]);
+    }
+    if (resultado.includes("X")) {
+      const resultadoFinal = resultado[0] * resultado[2];
+      setResultado([resultadoFinal]);
+    }
+    if (resultado.includes("/")) {
+      const resultadoFinal = resultado[0] / resultado[2];
+      setResultado([resultadoFinal]);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <View style={[styles.resultado, { flex: 1 }]}>
-        <Text>Aqui vai o resultado</Text>
+      <View style={[styles.resultado, { flex: 1, width: "100%" }]}>
+        <Text style={{fontSize: 30}}>{resultado}</Text>
       </View>
+      <View style={{ flex: 0.5 }} />
       <View style={{ flex: 3 }}>
-        <View style={[styles.botoes, { flex: 1 }]}>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>C</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>{''}</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>%</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>/</Button>
-        </View>
-        <View style={[styles.botoes, { flex: 1 }]}>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>7</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>8</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>9</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>X</Button>
-        </View>
-        <View style={[styles.botoes, { flex: 1 }]}>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>4</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>5</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>6</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>-</Button>
-        </View>
-        <View style={[styles.botoes, { flex: 1 }]}>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>1</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>2</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>3</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>+</Button>
-        </View>
-        <View style={[styles.botoes, { flex: 1 }]}>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>.</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>0</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>{'<'}</Button>
-          <Button mode="contained" style={styles.botao} onPress={console.log('clicou')}>
-            <Text style={{fontSize: 20}}>=</Text>
+        <View style={[styles.botoes, { flex: 0.5 }]}>
+          <Button
+            buttonColor="#2e2f38"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela("C")}
+          >
+            <Text fontSize={30}>C</Text>
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(0)}
+          >
+            0
+          </Button>
+          <Button
+            buttonColor="#2e2f38"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela("/")}
+          >
+            /
+          </Button>
+          <Button
+            buttonColor="#4b5efc"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela("X")}
+          >
+            X
           </Button>
         </View>
+        <View style={[styles.botoes, { flex: 0.5 }]}>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(7)}
+          >
+            7
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(8)}
+          >
+            8
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(9)}
+          >
+            9
+          </Button>
+          <Button
+            buttonColor="#4b5efc"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela("-")}
+          >
+            -
+          </Button>
+        </View>
+        <View style={[styles.botoes, { flex: 0.5 }]}>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(4)}
+          >
+            4
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(5)}
+          >
+            5
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(6)}
+          >
+            6
+          </Button>
+          <Button
+            buttonColor="#4b5efc"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela("+")}
+          >
+            +
+          </Button>
+        </View>
+        <View style={[styles.botoes, { flex: 0.5 }]}>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(1)}
+          >
+            1
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(2)}
+          >
+            2
+          </Button>
+          <Button
+            buttonColor="#d2d3da"
+            textColor="#000000"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => setResultadoTela(3)}
+          >
+            3
+          </Button>
+          <Button
+            buttonColor="#4b5efc"
+            mode="contained"
+            style={styles.botao}
+            onPress={() => calcularResultado()}
+          >
+            =
+          </Button>
+        </View>
+        <View style={{ flex: 0.4 }} />
       </View>
     </View>
   );
@@ -50,27 +208,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   resultado: {
-    borderWidth: 3,
-    borderColor: 'green'
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+    paddingRight: 20,
   },
   botoes: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     gap: 20,
   },
   botao: {
     width: 80,
     height: 80,
-    alignContent: 'center',
-    justifyContent: 'center',
-  }
+    alignContent: "center",
+    justifyContent: "center",
+  },
 });
